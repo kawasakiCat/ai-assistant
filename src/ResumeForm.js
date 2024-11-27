e165c3d73506a234b624d2c8f2d32edb9ce6ae52
sakura-1020 <sakura10k20@gmail.com>
2024-11-27 14:37:34 +0900

    履歴書フォーム（証明写真以外すべて）

--- src/ResumeForm.js
+++ src/ResumeForm.js
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 
 export function ResumeForm() {
     const [formData, setFormData] = useState({
-        lastname: '',
-        firstname: '',
-        lastname_kana: '',
-        firstname_kana: '',
-        birthdate: '',
+        lastName: '',
+        firstName: '',
+        lastNameKana: '',
+        firstNameKana: '',
+        birthDate: '',
         gender: '',
         email: '',
-        telno: '',
-        postalcode: '',
-        address_1: '',
-        address_2: '',
-        address_kana: '',
+        telNo: '',
+        postalCode: '',
+        address1: '',
+        address2: '',
+        addressKana: '',
         education: {
-            education_1: {
+            education1: {
                 year: '',
                 month: '',
                 schoolName: '',
@@ -24,7 +24,7 @@ export function ResumeForm() {
             },
         },
         workExperience: {
-            workExperience_1: {
+            workExperience1: {
                 year: '',
                 month: '',
                 companyName: '',
@@ -33,20 +33,42 @@ export function ResumeForm() {
             },
         },
         certification: {
-            certification_1: {
+            certification1: {
                 year: '',
                 month: '',
                 certification: '',
                 status: '',
             },
         },
+        motivation: '',
+        selfPromotion: '',
+        freeSpace: '',
+        submissionDate: '',
+
+    });
+    const [textCount, setTextCount] = useState({
+        motivation: 0,
+        selfPromotion: 0,
+        freeSpace: 0,
     });
     const [currentForm, setCurrentForm] = useState(0);
   
     //履歴書情報変更ハンドラー
     const handleFormData = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value})
-    }
+        if( e.target.name === 'motivation' || e.target.name === 'selfPromotion' ){
+            if( e.target.value.length <= 400 ){
+                setFormData({ ...formData, [e.target.name]: e.target.value});
+                setTextCount({ ...textCount, [e.target.name]: e.target.value.length});
+            }
+        }else if( e.target.name === 'freeSpace' ){
+            if( e.target.value.length <= 150 ){
+                setFormData({ ...formData, [e.target.name]: e.target.value});
+                setTextCount({ ...textCount, [e.target.name]: e.target.value.length});
+            }
+        }else{
+            setFormData({ ...formData, [e.target.name]: e.target.value});
+        };
+    };
 
     //学歴情報変更ハンドラー
     const handleFormGroup = (fieldGroup, key, field, value) => {
@@ -62,7 +84,7 @@ export function ResumeForm() {
 
     //学歴情報追加ハンドラー
     const addEducation = () => {
-        const nextKey = `education_${Object.keys(formData.education).length + 1}`;
+        const nextKey = `education${Object.keys(formData.education).length + 1}`;
         setFormData({ ...formData, education: 
             { ...formData.education,
                 [nextKey]: {
@@ -78,7 +100,7 @@ export function ResumeForm() {
 
     //職歴情報追加ハンドラー
     const addWorkExperience = () => {
-        const nextKey = `workExperience_${Object.keys(formData.workExperience).length + 1}`;
+        const nextKey = `workExperience${Object.keys(formData.workExperience).length + 1}`;
         setFormData({ ...formData, workExperience: 
             { ...formData.workExperience,
                 [nextKey]: {
@@ -94,7 +116,7 @@ export function ResumeForm() {
 
     //資格情報追加ハンドラー
     const addCertification = () => {
-        const nextKey = `certification_${Object.keys(formData.certification).length + 1}`;
+        const nextKey = `certification${Object.keys(formData.certification).length + 1}`;
         setFormData({ ...formData, certification: 
             { ...formData.certification,
                 [nextKey]: {
@@ -146,7 +168,9 @@ export function ResumeForm() {
             {currentForm === 1 && <ResumeForm2 data={formData} handleFormData={handleFormData} onNext={NextForm} onPrev={PrevForm} />}
             {currentForm === 2 && <ResumeForm3 data={formData} handleFormGroup={handleFormGroup} addFunction={addEducation} onNext={NextForm} onPrev={PrevForm} />}
             {currentForm === 3 && <ResumeForm4 data={formData} handleFormGroup={handleFormGroup} addFunction={addWorkExperience} onNext={NextForm} onPrev={PrevForm} />}
-            {currentForm === 4 && <ResumeForm5 data={formData} handleFormGroup={handleFormGroup} addFunction={addCertification} onNext={NextForm} onPrev={PrevForm} submit={handleSubmit} />}
+            {currentForm === 4 && <ResumeForm5 data={formData} handleFormGroup={handleFormGroup} addFunction={addCertification} onNext={NextForm} onPrev={PrevForm} />}
+            {currentForm === 5 && <ResumeForm6 data={formData} textCount={textCount} handleFormData={handleFormData} onNext={NextForm} onPrev={PrevForm} />}
+            {currentForm === 6 && <ResumeForm7 data={formData} textCount={textCount} handleFormData={handleFormData} onNext={NextForm} onPrev={PrevForm} submit={handleSubmit} />}
         </div>
     );
 }
@@ -156,28 +180,33 @@ function ResumeForm1({ data, handleFormData, onNext }) {
         <div>
             <div className='form-group'>
                 <label>姓</label>
-                <input type='text' name="lastname" placeholder=''
-                value={data.lastname} onChange={handleFormData} />
+                <input type='text' name="lastName" placeholder=''
+                value={data.lastName} onChange={handleFormData} />
             </div>
             <div>
                 <label>名</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="firstName" placeholder=''
+                value={data.firstName} onChange={handleFormData} />
             </div>
             <div>
                 <label>姓（かな）</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="lastNameKana" placeholder=''
+                value={data.lastNameKana} onChange={handleFormData} />
             </div>
             <div>
                 <label>名（かな）</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="firstNameKana" placeholder=''
+                value={data.firstNameKana} onChange={handleFormData} />
             </div>
             <div>
                 <label>生年月日</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="birthDate" placeholder=''
+                value={data.birthDate} onChange={handleFormData} />
             </div>
             <div>
                 <label>性別（任意）</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="gender" placeholder=''
+                value={data.gender} onChange={handleFormData} />
             </div>
             <button onClick={onNext}>次へ</button>
         </div>
@@ -196,27 +225,33 @@ function ResumeForm2({ data, handleFormData, onNext, onPrev}) {
         <div>
             <div className='form-group'>
                 <label>メールアドレス</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="email" placeholder=''
+                value={data.email} onChange={handleFormData} />
             </div>
             <div>
                 <label>電話番号</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="telNo" placeholder=''
+                value={data.telNo} onChange={handleFormData} />
             </div>
             <div>
                 <label>郵便番号</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="postalCode" placeholder=''
+                value={data.postalCode} onChange={handleFormData} />
             </div>
             <div>
                 <label>住所</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="address1" placeholder=''
+                value={data.address1} onChange={handleFormData} />
             </div>
             <div>
                 <label>住所（番地・建物名）</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="address2" placeholder=''
+                value={data.address2} onChange={handleFormData} />
             </div>
             <div>
                 <label>住所（かな）</label>
-                <input type='text' placeholder=''></input>
+                <input type='text' name="addressKana" placeholder=''
+                value={data.addressKana} onChange={handleFormData} />
             </div>
 
             <div>
@@ -338,7 +373,7 @@ function ResumeForm4({ data, handleFormGroup, addFunction, onNext, onPrev }) {
     );
 };
 
-function ResumeForm5({ data, handleFormGroup, addFunction, onNext, onPrev, submit }) {
+function ResumeForm5({ data, handleFormGroup, addFunction, onNext, onPrev }) {
     return (
         <div>
             {Object.keys(data.certification).map( (key, index) => (
@@ -365,33 +400,56 @@ function ResumeForm5({ data, handleFormGroup, addFunction, onNext, onPrev, submi
             <button onClick={addFunction}>免許・資格を追加</button>
             <button onClick={onNext}>次へ</button>
             <button onClick={onPrev}>戻る</button>
-            <button onClick={submit}>送信</button>
-
         </div>
     );
 };
 
-// function ResumeForm6({ data, handleFormData, onNext, onPrev, submit }) {
-//     return (
-//         <div>
-//             <div className='form-group'>
-//                 <label>志望動機</label>
-//                 <input type='text' name="lastname" placeholder=''
-//                 value={data.} onChange={handleFormData} />
-//             </div>
-//             <div>
-//                 <label>自己PR</label>
-//                 <input type='text' placeholder=''></input>
-//             </div>
-//             <div>
-//                 <label>姓（かな）</label>
-//                 <input type='text' placeholder=''></input>
-//             </div>
+function ResumeForm6({ data, textCount, handleFormData, onNext, onPrev }) {
+    
+    // const remainingMotivation = 400 - textCount.motivation;
+    // const remainingSelfPromotion = 400 - textCount.selfPromotion;
 
-//             <button onClick={onNext}>次へ</button>
-//             <button onClick={onPrev}>戻る</button>
-//             <button onClick={submit}>送信</button>
+    return (
+        <div>
+            <div className='form-group'>
+                <label>志望動機</label>
+                <input type='text' name="motivation" placeholder=''
+                value={data.motivation} onChange={handleFormData} />
+                <div>{textCount.motivation} / 400</div>
+                {/* <p><div style={{ color: remainingMotivation < 10 ? 'yellow' : 'black' }}>{textCount.selfPromotion}</div> / 400</p> */}
+            </div>
+            <div>
+                <label>自己PR</label>
+                <input type='text' name="selfPromotion" placeholder=''
+                value={data.selfPromotion} onChange={handleFormData} />
+                <div>{textCount.selfPromotion} / 400</div>
+                {/* <p><div style={{ color: remainingSelfPromotion < 10 ? 'yellow' : 'black' }}>{textCount.selfPromotion}</div> / 400</p> */}
+            </div>
 
-//         </div>
-//     );
-// };
+            <button onClick={onNext}>次へ</button>
+            <button onClick={onPrev}>戻る</button>
+        </div>
+    );
+};
+
+function ResumeForm7({ data, textCount, handleFormData, onNext, onPrev, submit }) {
+    return (
+        <div>
+            <div className='form-group'>
+                <label>本人希望欄</label>
+                <input type='text' name="freeSpace" placeholder=''
+                value={data.freeSpace} onChange={handleFormData} />
+                <div>{textCount.freeSpace} / 150</div>
+            </div>
+            <div>
+                <label>履歴書提出日付</label>
+                <input type='text' name="submissionDate" placeholder=''
+                value={data.submissionDate} onChange={handleFormData} />
+            </div>
+
+            <button onClick={onNext}>プレビュー画面へ</button>
+            <button onClick={submit}>送信</button>
+            <button onClick={onPrev}>戻る</button>
+        </div>
+    );
+};
