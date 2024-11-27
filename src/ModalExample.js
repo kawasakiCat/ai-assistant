afbb2cfa1fd2ab34f1f92d78490122fab0369dac
oimomin <mino001sato@gmail.com>
2024-11-27 12:14:47 +0900

    InputとModalの使用例

--- src/ModalExample.js
+++ src/ModalExample.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Modal from '../../../components/common/Modal/Modal';
-import Button from '../../../components/common/Button/Button';
+import Modal from './components/common/Modal/Modal';
+import Button from './components/common/Button/Button';
 
 const ModalExample = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
