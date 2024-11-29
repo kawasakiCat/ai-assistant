1da96e888baa284b53da2c9f8351881846c19ebc
oimomin <mino001sato@gmail.com>
2024-11-29 10:51:09 +0900

    志望動機フォーム

--- src/features/chat/components/ChatForm.js
+++ src/features/chat/components/ChatForm.js
@@ -24,16 +24,54 @@ const ChatFormComponent = () => {
 				label="志望業界"
 				name="targetIndustry"
 				value={formData.name}
-				onChange={handleInputChange}
+        onChange={handleInputChange}
 				required
 				helperText="例: 情報通信業"
 			/>
+      <Input
+				type="text"
+				label="志望企業"
+				name="targetCompany"
+				value={formData.targetCompany}
+        onChange={handleInputChange}
+				helperText="例: 学校法人コア学園"
+			/>
 			<TextArea
 				type="text"
-				label="目指すきっかけ"
+				label="あなたの背景"
+        name="personalBackground"
+        value={formData.personalBackground}
+        onChange={handleInputChange}
+        helperText="あなたの学生時代の専門分野や、インターンシップの経験、資格や技能、興味のある技術や分野など"
+        rows={2}
 			/>
-      <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
-      <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
+      <TextArea
+				type="text"
+				label="キャリア志向"
+        name="careerGoals"
+        value={formData.careerGoals}
+        onChange={handleInputChange}
+        helperText="将来のキャリアビジョン、この企業・業界で実現したいこと、貢献できる具体的な能力など"
+        rows={2}
+			/>
+      <TextArea
+				type="text"
+				label="企業への理解"
+        name="companyUnderstanting"
+        value={formData.companyUnderstanting}
+        onChange={handleInputChange}
+        helperText="企業の事業内容、企業の理念や文化、注目している製品やサービス、企業の社会的意義や将来性、企業の特徴や強みなど"
+        rows={2}
+			/>
+      <TextArea
+				type="text"
+				label="マッチング要素"
+        name="matchingElements"
+        value={formData.matchingElements}
+        onChange={handleInputChange}
+        helperText="企業の事業と自身の興味の接点、企業文化と自身の価値観の一致点、なぜこの企業を選んだか、企業に対する具体的な興味や共感点など"
+        rows={2}
+			/>
       <Button type="submit">送信</Button>
     </form>
   );
