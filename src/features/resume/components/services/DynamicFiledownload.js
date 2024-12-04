3e2f5c60a830f414a8a79c0e7d22333001d09b22
oimomin <mino001sato@gmail.com>
2024-12-04 13:02:19 +0900

    ダウンロードボタン/エラーが13個あります

---
+++ src/features/resume/components/services/DynamicFiledownload.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// import axios from 'axios';
+import Button from '../../../../components/common/Button/Button';
+
+const DynamicFileDownload = (data) => {
+  const handleDownload = async () => {
+	// APIからデータを取得
+    const data = await fetch("/api/resume"), {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json;charset=UTF-8",
+		},
+		body: JSON.stringify({name: name,})
+	})
+    const blob = new Blob([data], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = 'out.xxlsx'; // ダウンロード時のファイル名
+    anchor.click();
+
+    // メモリリークを防ぐためURLを解放
+    URL.revokeObjectURL(url);
+  };
+
+  return (
+    <Button onClick={handleDownload}>ダウンロード</Button>
+  );
+};
+
+export default DynamicFileDownload;
