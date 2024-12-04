5c2d672709a192a984d69075609be1488e2dc4dc
oimomin <mino001sato@gmail.com>
2024-12-04 22:12:42 +0900

    バグの温床をコメントアウト

--- src/features/resume/components/services/DynamicFiledownload.js
+++ src/features/resume/components/services/DynamicFiledownload.js
@@ -1,32 +1,32 @@
-import React from 'react';
-// import axios from 'axios';
-import Button from '../../../../components/common/Button/Button';
+//import React from 'react';
+//// import axios from 'axios';
+//import Button from '../../../../components/common/Button/Button';
 
-const DynamicFileDownload = (data) => {
-  const handleDownload = async () => {
-	// APIからデータを取得
-    const data = await fetch("/api/resume"), {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json;charset=UTF-8",
-		},
-		body: JSON.stringify({name: name,})
-	})
-    const blob = new Blob([data], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
+//const DynamicFileDownload = (data) => {
+//  const handleDownload = async () => {
+//	// APIからデータを取得
+//    const data = await fetch("https://ai-assistant.core-akita.ac.jp/api/resume/excel"), {
+//		method: "POST",
+//		headers: {
+//			"Content-Type": "application/json"
+//		},
+//		body: JSON.stringify({name: name,})
+//	}
+//    const blob = new Blob([data], { type: 'text/plain' });
+//    const url = URL.createObjectURL(blob);
 
-    const anchor = document.createElement('a');
-    anchor.href = url;
-    anchor.download = 'out.xxlsx'; // ダウンロード時のファイル名
-    anchor.click();
+//    const anchor = document.createElement('a');
+//    anchor.href = url;
+//    anchor.download = 'out.xxlsx'; // ダウンロード時のファイル名
+//    anchor.click();
 
-    // メモリリークを防ぐためURLを解放
-    URL.revokeObjectURL(url);
-  };
+//    // メモリリークを防ぐためURLを解放
+//    URL.revokeObjectURL(url);
+//  };
 
-  return (
-    <Button onClick={handleDownload}>ダウンロード</Button>
-  );
-};
+//  return (
+//    <Button onClick={handleDownload}>ダウンロード</Button>
+//  );
+//};
 
-export default DynamicFileDownload;
+//export default DynamicFileDownload;
