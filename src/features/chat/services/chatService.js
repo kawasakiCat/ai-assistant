9aa1fd7ba0a94ca9f835434c940603c4cc23acef
oimomin <mino001sato@gmail.com>
2024-12-04 12:03:16 +0900

    チャットのAPI呼び出し

--- src/features/chat/services/chatService.js
+++ src/features/chat/services/chatService.js
@@ -1,22 +1,22 @@
-// const API_BASE_URL = "https://ai-assistant.core-akita.ac.jp/api"; // 例: 本番用URL
+const API_BASE_URL = "https://ai-assistant.core-akita.ac.jp/api"; // 例: 本番用URL
 
 // 志望動機を生成するAPI
 export async function submitMotivationForm(data) {
   try {
-    // const response = await fetch(`${API_BASE_URL}/motivation`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(data)
-    // });
+    const response = await fetch(`${API_BASE_URL}/motivation`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
 
-    // if (!response.ok) {
-    //   throw new Error(`HTTPエラー! ステータス: ${response.status}`);
-    // }
+    if (!response.ok) {
+      throw new Error(`HTTPエラー! ステータス: ${response.status}`);
+    }
 
-    // const result = await response.json();
-	  const result = "APIに送信する関数を呼び出しました";
+    const result = await response.json();
+	  // const result = "APIに送信する関数を呼び出しました";
     return result;
 	} catch (error) {
 		console.error("API呼び出し中にエラーが発生しました:", error);
